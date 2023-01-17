24978ac4e55cd91abfa88d6ebfc6e841c5c43349
SeoungWoo <tjddnxodzm12@naver.com>
2023-01-17 13:25:05 +0900

    Use DefaultTabController

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,45 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TabBarDemo());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TabBarDemo extends StatelessWidget {
+  const TabBarDemo({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.black,
-      ),
-      home: const Main(),
-    );
-  }
-}
-
-class Main extends StatelessWidget {
-  const Main({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: const Text(
-          "무제",
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w600,
-            fontFamily: "NewFont",
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            centerTitle: true,
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                    icon: Icon(
+                  Icons.yard_outlined,
+                  color: Colors.black,
+                )),
+                Tab(
+                    icon: Icon(
+                  Icons.checklist_rtl_outlined,
+                  color: Colors.black,
+                )),
+                Tab(
+                    icon: Icon(
+                  Icons.pending_actions_sharp,
+                  color: Colors.black,
+                )),
+              ],
+            ),
+            title: Text(
+              'How About Your Today?',
+              style: TextStyle(
+                color: Colors.blue.shade300,
+                fontWeight: FontWeight.w600,
+                fontFamily: "NewFont",
+              ),
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Image.asset("assets/images/flower.png"),
+              const Icon(Icons.directions_transit),
+              const Icon(Icons.directions_bike),
+            ],
           ),
         ),
-      ),
-      body: Center(
-        child: Image.asset("assets/images/flower.png"),
       ),
     );
   }
