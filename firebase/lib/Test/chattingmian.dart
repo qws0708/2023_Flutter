a028cf41a65b88734d005c9b99b43cbf664f51de
SeoungWoo <tjddnxodzm12@naver.com>
2023-01-31 16:46:30 +0900

    StatefulWidget with Firebase (plus and minus)

--- firebase/lib/Test/chattingmian.dart
+++ firebase/lib/Test/chattingmian.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase/screen/message_list_screen.dart';
+import 'package:firebase/test/screen/message_list_screen.dart';
 
 //import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
